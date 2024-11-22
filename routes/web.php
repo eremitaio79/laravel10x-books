d3bb05acaee4db8c4cbf9ca8ea4f934bdfe92c38
Paulo Eremita <eremitaio79@gmail.com>
2024-11-22 11:46:17 -0300

    Included adminer and tests routes

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,25 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+// My routes.
+// Teste1.
+Route::get('/test.index', function() {
+    return view('tests.index');
+})->name('hh');
+
+// Teste2.
+Route::get('/test.bola', function() {
+    return view('tests.bola');
+})->name('bolabola');
+
+// Rota do tipo any: aceita qualquer tipo de verbo http na rota.
+Route::any('/any', function() {
+    // return view('tests.bola');
+    return 'Anything!!!';
+});
+
+// Rota do tipo match: neste tipo é obrigatório informar quais os verbos https são permitidos.
+Route::match(['get', 'post'], '/match', function() {
+    return 'Match route!!!';
+});
